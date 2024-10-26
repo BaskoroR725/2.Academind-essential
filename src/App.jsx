682d5b76199dcf8828fa817e131d833c5bd7f765
Baskoro Ramadhan <baskoror725@gmail.com>
2024-10-26 22:30:14 +0700

    Adding props and data props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import reactImg from './assets/react-core-concepts.png'
+import componentsImg from './assets/components.png'
+import { CORE_CONCEPTS } from './data';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcept({image,title,description}){
+  return(
+    <li>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+    </li>
+  )
 }
 
 function Header() {
@@ -24,6 +36,23 @@ function App() {
     <div>
       <Header />
       <main>
+        <section id='core-concepts'>
+          <h2>Core Concept</h2>
+          <ul>
+            <CoreConcept 
+              title={CORE_CONCEPTS[0].title}
+              description= {CORE_CONCEPTS[0].description}
+              image = {CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept 
+              title={CORE_CONCEPTS[1].title}
+              description= {CORE_CONCEPTS[1].description}
+              image = {CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
